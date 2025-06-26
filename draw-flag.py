cfb9b7f2ca20bd1918e5c1426a7ea1af18484e44
Sohayel Mahmud <132913772+sohayelmahmud@users.noreply.github.com>
2025-06-26 20:43:36 +0600

    Draw Flag

---
+++ draw-flag.py
@@ -0,0 +1,16 @@
+from graphics import Canvas
+import random
+
+CANVAS_WIDTH = 450
+CANVAS_HEIGHT = 300
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    # TODO, your code here
+    canvas.create_rectangle(0, 0, 450, 150, "red")
+    
+    
+    
+
+if __name__ == '__main__':
+    main()
